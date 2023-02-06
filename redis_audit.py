4e6050d66ecd5e3da0dae0a629a8a46d4d062476
Jeremy Lumley <jlumley@tuwcows.com>
2023-02-06 16:08:35 -0500

    redis audit

--- redis_audit.py
+++ redis_audit.py
@@ -29,7 +29,7 @@ for key in all_keys:
     if namespace not in key_namespaces.keys():
         key_namespaces[namespace] = list()
     
-    dbg = client.debug_object(key, target_nodes=Redis.ALL_NODES)
+    dbg = client.memory_usage(key) 
     print(dbg)
     raise
 
