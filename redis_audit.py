70f391fee139d1959973d004cfd466c32480eac2
Jeremy Lumley <jlumley@tuwcows.com>
2023-02-07 10:47:20 -0500

    redis audit

--- redis_audit.py
+++ redis_audit.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-import argparse
+import argparse 
 import re
 
+from multiprocessing import Process
 from redis.cluster import RedisCluster as Redis
 from redis.cluster import ClusterNode as Node
 
@@ -14,12 +15,6 @@ parser.add_argument('port', type=str)
 parser.add_argument('password', type=str)
 args = parser.parse_args()
 
-startup_nodes = list()
-key_namespaces = dict()
-total_memory_used = 0
-namespace_regex = re.compile(".*:")
-
-
 def sizeof_fmt(num, suffix="B"):
     for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
         if abs(num) < 1024.0:
@@ -27,33 +22,54 @@ def sizeof_fmt(num, suffix="B"):
         num /= 1024.0
     return f"{num:.1f}Yi{suffix}"
 
-for node in range(1, args.node_count+1):
-    startup_nodes.append(Node(args.host+str(node).zfill(3), args.port))
-
-client = Redis(startup_nodes=startup_nodes, password=args.password)
-
-all_keys = client.keys()
-num_keys = len(all_keys)
-for i in range(num_keys):
-    if (i%1000 == 100):
-        break
-        print(f"working on key {i} of {num_keys}")
-    key = all_keys[i].decode("utf-8")
-    namespace = namespace_regex.search(key).group(0)
-    if namespace not in key_namespaces.keys():
-        key_namespaces[namespace] = 0 
-    
-    bytes_used = client.memory_usage(key) 
-    total_memory_used += bytes_used
-    key_namespaces[namespace] += bytes_used
-
-
-for namespace in key_namespaces.keys():
-    size = sizeof_fmt(key_namespaces[namespace])
-    print(f"{namespace} | {size} | {round(key_namespaces[namespace]/total_memory_used,2)}%")
-
-print()
-print(f"total bytes used: {sizeof_fmt(total_memory_used)}")
 
 
 
+def audit_redis(keys):
+
+    startup_nodes = list()
+    key_namespaces = dict(total=0)
+    namespace_regex = re.compile(".*:")
+
+    for node in range(1, args.node_count+1):
+        startup_nodes.append(Node(args.host+str(node).zfill(3), args.port))
+
+    client = Redis(startup_nodes=startup_nodes, password=args.password)
+
+    num_keys = len(keys)
+    for i in range(num_keys):
+        if (i%1000 == 0):
+            print(f"working on key {i} of {num_keys}")
+        key = keys[i].decode("utf-8")
+        namespace = namespace_regex.search(key).group(0)
+        if namespace not in key_namespaces.keys():
+            key_namespaces[namespace] = 0 
+        
+        bytes_used = client.memory_usage(key) 
+        key_namespaces['total'] += bytes_used
+        key_namespaces[namespace] += bytes_used
+
+
+    for namespace in key_namespaces.keys():
+        size = sizeof_fmt(key_namespaces[namespace])
+        print(f"{namespace} | {size} | {round(key_namespaces[namespace]/key_namespaces['total'],2)}%")
+
+    return key_namespaces
+
+
+def main():
+
+    for node in range(1, args.node_count+1):
+        startup_nodes.append(Node(args.host+str(node).zfill(3), args.port))
+
+    client = Redis(startup_nodes=startup_nodes, password=args.password)
+    all_keys = client.keys()
+
+    list_of_keys = all_keys[:1000]
+    p = Process(target=audit_redis, args=(list_of_keys,))
+    p.start()
+    p.join()
+    print('done')
+
+if __name__ == "__main__":
+    main()
