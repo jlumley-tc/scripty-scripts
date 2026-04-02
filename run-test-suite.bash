961e66df6e97656a7ab7f0b12f6e1a4fc6335077
Jeremy Lumley <jlumley@tucows.com>
2026-04-02 15:07:26 -0400

    test suite

--- run-test-suite.bash
+++ run-test-suite.bash
@@ -1,23 +1,22 @@
 
 # uv run ./es_performance_transactions.py --accounts-file accounts --root-size 25 --range 1m
-# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 25 --range 3m
-# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 25 --range 6m
-uv run ./es_performance_transactions.py --accounts-file accounts --root-size 25 --range 12m
-
-
-# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 50 --range 1m
-# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 50 --range 3m
-# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 50 --range 6m
-uv run ./es_performance_transactions.py --accounts-file accounts --root-size 50 --range 12m
-
-
+uv run ./es_performance_transactions.py --accounts-file accounts --root-size 50 --range 1m
 # uv run ./es_performance_transactions.py --accounts-file accounts --root-size 100 --range 1m
-# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 100 --range 3m
-# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 100 --range 6m
-uv run ./es_performance_transactions.py --accounts-file accounts --root-size 100 --range 12m
-
-
 # uv run ./es_performance_transactions.py --accounts-file accounts --root-size 200 --range 1m
+
+
+# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 25 --range 3m
+uv run ./es_performance_transactions.py --accounts-file accounts --root-size 50 --range 3m
+# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 100 --range 3m
 # uv run ./es_performance_transactions.py --accounts-file accounts --root-size 200 --range 3m
+
+
+# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 25 --range 6m
+uv run ./es_performance_transactions.py --accounts-file accounts --root-size 50 --range 6m
+# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 100 --range 6m
 # uv run ./es_performance_transactions.py --accounts-file accounts --root-size 200 --range 6m
-uv run ./es_performance_transactions.py --accounts-file accounts --root-size 200 --range 12m
+
+# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 25 --range 12m
+uv run ./es_performance_transactions.py --accounts-file accounts --root-size 50 --range 12m
+# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 100 --range 12m
+# uv run ./es_performance_transactions.py --accounts-file accounts --root-size 200 --range 12m
